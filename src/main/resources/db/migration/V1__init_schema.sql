bf44bc46fff903445c405d2e4828b492de6fbfa3
BEKSULTAN <230599@astanait.edu.kz>
2025-06-09 15:53:29 +0500

    v1 migration added

---
+++ src/main/resources/db/migration/V1__init_schema.sql
@@ -0,0 +1,77 @@
+CREATE TABLE users (
+   id UUID PRIMARY KEY,
+   first_name VARCHAR(255),
+   last_name VARCHAR(255),
+   email VARCHAR(255) UNIQUE NOT NULL,
+   password VARCHAR(255) NOT NULL,
+   role VARCHAR(50) NOT NULL,
+   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+   updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+   is_active BOOLEAN NOT NULL DEFAULT TRUE,
+);
+
+CREATE TABLE IF NOT EXISTS books (
+    id UUID PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    author VARCHAR(255) NOT NULL,
+    isbn VARCHAR(20) UNIQUE,
+    publisher VARCHAR(100),
+    published_year INT,
+    genre VARCHAR(50),
+    description TEXT,
+    total_copies INT NOT NULL DEFAULT 1,
+    available_copies INT NOT NULL DEFAULT 1
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS reservations (
+      id UUID PRIMARY KEY,
+      user_id UUID NOT NULL,
+      book_id UUID NOT NULL,
+      status VARCHAR(20) NOT NULL,
+      reserved_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+      cancelled_at TIMESTAMP,
+      expires_at TIMESTAMP NOT NULL,
+      FOREIGN KEY (user_id) REFERENCES users(id),
+      FOREIGN KEY (book_id) REFERENCES books(id)
+);
+
+CREATE TABLE IF NOT EXISTS loan (
+    id UUID PRIMARY KEY,
+    user_id UUID NOT NULL,
+    book_id UUID NOT NULL,
+    issued_by UUID REFERENCES users(id),
+    issue_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    return_date TIMESTAMP,
+    due_date TIMESTAMP NOT NULL,
+    status ENUM('BORROWED', 'RETURNED') NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (book_id) REFERENCES books(id)
+);
+
+CREATE TABLE IF NOT EXISTS feedback (
+    id UUID PRIMARY KEY,
+    user_id UUID NOT NULL,
+    book_id UUID NOT NULL,
+    rating INT,
+    comment TEXT,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (book_id) REFERENCES books(id)
+);
+
+CREATE TABLE IF NOT EXISTS favoritebooks (
+    id UUID PRIMARY KEY,
+    user_id UUID NOT NULL,
+    book_id UUID NOT NULL,
+    UNIQUE (user_id, book_id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (book_id) REFERENCES books(id)
+);
+
+CREATE INDEX idx_reservations_user_id ON reservations(user_id);
+CREATE INDEX idx_reservations_book_id ON reservations(book_id);
