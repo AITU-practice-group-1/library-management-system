b3d5516a9988645b3adc68ed75d844a26373a3e2
Ali <suhansun13@gmail.com>
2025-06-16 13:29:06 +0500

    Added a new feature - review reports. Users can report a review, it will come out in admin's dashboard and the admin will decide whether or not to delete that review. New view: reviewReports.html, and changed the _reviews.html to have a new button to report.

---
+++ src/main/java/com/example/librarymanagementsystem/Services/impl/ReviewReportServiceImpl.java
@@ -0,0 +1,85 @@
+package com.example.librarymanagementsystem.Services.impl;
+
+import com.example.librarymanagementsystem.Entities.Feedback;
+import com.example.librarymanagementsystem.Entities.ReviewReport;
+import com.example.librarymanagementsystem.Entities.User;
+import com.example.librarymanagementsystem.Repositories.FeedbackRepository;
+import com.example.librarymanagementsystem.Repositories.ReviewReportRepository;
+import com.example.librarymanagementsystem.Services.FeedbackService;
+import com.example.librarymanagementsystem.Services.ReviewReportService;
+import com.example.librarymanagementsystem.util.ReviewReportStatus;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.flywaydb.core.internal.util.JsonUtils;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class ReviewReportServiceImpl implements ReviewReportService {
+    private final ReviewReportRepository reviewReportRepository;
+    private final FeedbackRepository feedbackRepository;
+
+
+    @Override
+    @Transactional(readOnly = true)
+    public Page<ReviewReport> getAllReviewReport(Pageable pageable) {
+        return reviewReportRepository.findAll(pageable);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Page<ReviewReport> getAllReviewReports(ReviewReportStatus status, Pageable pageable) {
+        return reviewReportRepository.findAllByStatus(status, pageable);
+    }
+
+
+    @Override
+    public ReviewReport createReviewReport(UUID feedbackId, User reportingUser) {
+        Feedback feedbackToReport = feedbackRepository.findById(feedbackId)
+                .orElseThrow(() -> new EntityNotFoundException("Feedback not found" + feedbackId));
+        if (reviewReportRepository.existsByFeedbackAndUser(feedbackToReport, reportingUser)) {
+            throw new IllegalArgumentException("You have already reported this review");
+        }
+
+        ReviewReport newReport = new ReviewReport();
+        newReport.setFeedback(feedbackToReport);
+        newReport.setUser(reportingUser);
+        newReport.setStatus(ReviewReportStatus.PENDING);
+
+        return reviewReportRepository.save(newReport);
+    }
+
+    @Override
+    @Transactional
+    public void approveReviewReport(UUID reportId) {
+        ReviewReport approvedReport = reviewReportRepository.findById(reportId)
+                .orElseThrow(() -> new EntityNotFoundException("ReviewReport not found with id: " + reportId));
+
+        Feedback feedbackToDelete = approvedReport.getFeedback();
+
+        if (feedbackToDelete == null) {
+            reviewReportRepository.delete(approvedReport);
+            return;
+        }
+
+        // Before deleting the report, delete all the reports to a feedback
+        reviewReportRepository.deleteAllByFeedback(feedbackToDelete);
+
+        feedbackRepository.delete(feedbackToDelete);
+    }
+
+
+    @Override
+    public void rejectReviewReport(UUID id) {
+        ReviewReport report = reviewReportRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Review report not found"));
+        report.setStatus(ReviewReportStatus.REJECTED);
+        reviewReportRepository.save(report);
+    }
+}
